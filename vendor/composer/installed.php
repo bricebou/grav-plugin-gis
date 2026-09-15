667682bb5262a2c8dac1c579a40b03d7effa1e1d
bricebou <bricebou@momh.fr>
2026-09-15 22:37:53 +0200

    chore: vendor folder needs to be bundled with the plugin...
    Fix: #1,#2

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'bricebou/gis',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b2a2816b5d618ec59800c0d120a4b8b1fdd629f9',
+        'type' => 'grav-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'bricebou/gis' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b2a2816b5d618ec59800c0d120a4b8b1fdd629f9',
+            'type' => 'grav-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
